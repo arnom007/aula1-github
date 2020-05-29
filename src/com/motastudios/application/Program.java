2f39a093e5a85a046fd91629ec5627cb383aee68
arnom007 <arnomthg@gmail.com>
2020-05-29 20:32:23 -0300

    Acrescentei um receptor de dados e um Sysout

--- src/com/motastudios/application/Program.java
+++ src/com/motastudios/application/Program.java
@@ -1,5 +1,17 @@
 package com.motastudios.application;
 
-public class Program {
+import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(JOptionPane.showInputDialog(""));
+		String hi = sc.nextLine();
+		System.out.println(hi);
+		sc.close();
+		System.exit(0);
+		
+	}
 }
